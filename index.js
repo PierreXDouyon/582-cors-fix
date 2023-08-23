348da458a2b2a33c19730b0511cdd9d312cba7f5
VANIERCOLLEGE\2295472 <2295472@edu.vaniercollege.qc.ca>
2023-08-23 16:55:07 -0400

    updrated index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
 // Replace the uri string with your connection string.
 const uri =
-  "INSERT YOUR MONGODB CONNECTION STRING HERE";
+  "mongodb+srv://2295472:pNmgIscXfjESkutj@cluster0.9jqfmwc.mongodb.net/?retryWrites=true&w=majority";
 
 // parse application/json
 app.use(bodyParser.json());
